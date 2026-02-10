cfbaf61957e3b2c541b2ff3f8c7a18e264f645f0
mdsamiulislam54 <samiulm5332@gmail.com>
2026-02-10 10:27:20 +0600

    prepare backend for deploymen

--- src/helper/timeStringToDate.ts
+++ src/helper/timeStringToDate.ts
@@ -1,5 +1,5 @@
 export const timeStringToDate = (time: string) => {
-  const [hour, minute] = time.split(":").map(Number)
+  const [hour=0, minute=0] = time.split(":").map(Number)
 
   const date = new Date()
   date.setHours(hour, minute, 0, 0)
